896ee9030561b70764300eae9ab6f2d4333d4584
Srikanth Kunkulagunta <srikanth.kunkulagunta@gmail.com>
2015-02-06 23:33:22 +0000

    cells can identify neighbours

--- game_of_life_spec.rb
+++ game_of_life_spec.rb
@@ -4,12 +4,63 @@ class Cell
   def initialize(game,x,y)
     @x=x
     @y=y
+    @game=game
 
-    game.cells << self
+    @game.cells << self
   end
 
   def neighbours
-    []
+    [north, north_east,east, south_east, south, south_west, west, north_west].select do |neighbour|
+      neighbour == true
+    end
+  end
+
+  def north
+    game.cells.select do |cell|
+      cell.x == self.x && cell.y == self.y+1
+    end.any?
+  end
+
+  def north_east
+    game.cells.select do |cell|
+      cell.x == self.x+1 && cell.y == self.y+1
+    end.any?
+  end
+
+  def east
+    game.cells.select do |cell|
+      cell.x == self.x+1 && cell.y == self.y
+    end.any?
+  end
+
+  def south_east
+    game.cells.select do |cell|
+      cell.x == self.x+1 && cell.y == self.y-1
+    end.any?
+  end
+
+  def south
+    game.cells.select do |cell|
+      cell.x == self.x && cell.y == self.y-1
+    end.any?
+  end
+
+  def south_west
+    game.cells.select do |cell|
+      cell.x == self.x-1 && cell.y == self.y-1
+    end.any?
+  end
+
+  def west
+    game.cells.select do |cell|
+      cell.x == self.x-1 && cell.y == self.y
+    end.any?
+  end
+
+  def north_west
+    game.cells.select do |cell|
+      cell.x == self.x-1 && cell.y == self.y+1
+    end.any?
   end
 
   class << self
@@ -36,10 +87,18 @@ class Game
 end
 
 describe 'game of life' do
-  subject{ Cell.spawn_at(1,2) }
   before{ Game.setup }
 
+  describe 'Game' do
+    subject{ Game.new }
+    it 'contains cells' do
+      expect(subject.cells.count).to eq 0
+    end
+  end
+
   describe Cell do
+    subject{ Cell.spawn_at(1,2) }
+
     it 'belongs to a game' do
       expect(Cell.game).to be_a Game
     end
@@ -53,18 +112,49 @@ describe 'game of life' do
       expect(subject.neighbours.count).to eq 0
     end
 
-    # context 'identifying neighbours' do
-    #   it 'to the east' do
-    #     Cell.spawn_at(2,2)
-    #     expect(subject.neighbours.count).to eq 1
-    #   end
-    # end
-  end
-end
+    context 'identifying neighbours' do
+      it 'to the north' do
+        Cell.spawn_at(1,3)
+        expect(subject.neighbours.count).to eq 1
+      end
 
-describe 'Game' do
-  subject{ Game.new }
-  it 'contains cells' do
-    expect(subject.cells.count).to eq 0
+      it 'to the north-east' do
+        Cell.spawn_at(2,3)
+        expect(subject.neighbours.count).to eq 1
+      end
+
+      it 'to the east' do
+        Cell.spawn_at(2,2)
+        expect(subject.neighbours.count).to eq 1
+      end
+
+      it 'to the south-east' do
+        Cell.spawn_at(2,1)
+        expect(subject.neighbours.count).to eq 1
+      end
+
+      it 'to the south' do
+        Cell.spawn_at(1,1)
+        expect(subject.neighbours.count).to eq 1
+      end
+
+      it 'to the south_west' do
+        Cell.spawn_at(0,1)
+        expect(subject.neighbours.count).to eq 1
+      end
+
+      it 'to the west' do
+        Cell.spawn_at(0,2)
+        expect(subject.neighbours.count).to eq 1
+      end
+
+      it 'to the north_west' do
+        Cell.spawn_at(0,3)
+        expect(subject.neighbours.count).to eq 1
+      end
+    end
+  end
+
+  it 'rule 1: Any live cell with fewer than two live neighbours dies' do
   end
 end
